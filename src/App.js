a9596fd7dac72d8379fc71f14e1ad9bee82166bf
luoqi <luoq366@aliyun.com>
2021-05-30 13:54:05 +0800

    框架

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
-import { Button } from 'antd';
-import './App.css';
+import React,  { Component } from 'react';
+import Routers from '@/router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button>测试</Button>
-    </div>
-  );
+class App extends Component {
+  state = {
+    title: ''
+  }
+  setTitle = ({ title }) => {
+    if(this.state.title === title) return;
+    // this.setState({ title });
+  }
+  render() {
+    return(
+      <div className="App">
+        <Routers onRouterChange={this.setTitle}></Routers>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/router.js
@@ -0,0 +1,7 @@
+import { Router, Route, hashHistory } from 'react-router';
+
+<Router history={hashHistory}>
+  <Route path="/" component={App}/>
+  <Route path="/repos" component={Repos}/>
+  <Route path="/about" component={About}/>
+</Router>
